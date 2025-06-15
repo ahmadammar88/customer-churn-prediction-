452ccf534e61524eb5bf00a4d2ab644c77bbef96
ahmadammar88 <aaahmadammar88@gmail.com>
2025-06-15 13:19:22 -0700

    Remove all joblib references, use only pickle for model persistence

--- train_model.py
+++ train_model.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-import joblib
 import mlflow
 import mlflow.sklearn
 import pickle
